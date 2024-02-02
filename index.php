f678f2130e4149f140da0cddc34abe6999f469ca
trtonmoy <tr.tonmoy0110.trt@gmail.com>
2024-02-03 05:35:38 +0600

    woss repo

---
+++ index.php
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<feed xmlns="http://www.w3.org/2005/Atom" xml:lang="en-gb">
+	<title type="text">Andy the Wisguy</title>
+	<subtitle type="text">Slacker roommates Andy and Pepe are comical vignettes combining laconic psychedelia, childlike enchantment, drug-fueled hedonism, and impish mischief.</subtitle>
+	<link rel="alternate" type="text/html" href="https://boysclubandy.com"/>
+	<id>https://boysclubandy.com/index.php</id>
+	<updated>2023-08-28T03:29:00+00:00</updated>
+	<generator uri="https://www.joomla.org">Joomla! - Open Source Content Management</generator>
+	<link rel="self" type="application/atom+xml" href="https://boysclubandy.com/index.php?format=feed&amp;type=atom"/>
+</feed>
